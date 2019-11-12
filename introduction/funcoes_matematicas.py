4020a6512457ae4c61ad948f502dcbb8e3318623
Dborah <deborahferreirafonseca@gmail.com>
2019-11-12 07:25:14 -0300

    funcoes matemáticas

---
+++ introduction/funcoes_matematicas.py
@@ -0,0 +1,32 @@
+import math
+
+#maior inteiro maior ou menor a um determinado numero
+print(math.ceil(3.14))
+
+#valor absoluto de um numero
+print(math.fabs(-4))
+
+#maior inteiro menor ou igual a um determinado número
+print(math.floor(3.14))
+
+#resultado da exponenciacao
+print(math.exp(2))
+
+#logaritmo
+print(math.log10(10))
+
+#potencia
+print(math.pow(2,3))
+
+#raiz quadrada
+print(math.sqrt(25))
+
+#cosseno
+print(math.cos(0))
+
+#seno
+print(math.sin(30))
+
+
+
+
